f9f2c5f3b5976c17c6bd9b4cff08b0f908b8f03e
Nicolai800 <happy455420@gmail.com>
2024-12-01 00:24:09 +0100

    cleaned up the code

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import styles from "./ProgectsStyles.module.css";
 import ProjectCard from "../../common/ProjectCard";
-// import artKissDesignLogo from "../../assets/artKissLogo.png";
-// import gardenLogo from "../../assets/gardenIcon.png";
-// import pongGameLogo from "../../assets/machine.png";
 import projectsData from "../../data/projectsDataArray";
 
 const Projects = () => {
@@ -21,25 +18,6 @@ const Projects = () => {
               description={project.description}
             />
           ))}
-          {/* <ProjectCard
-            src={artKissDesignLogo}
-            link="https://artkissdesign.pl/"
-            title="Art Kiss Design"
-            description="Online store(commercial)"
-          />
-          <ProjectCard
-            src={gardenLogo}
-            link="https://store-e1th.onrender.com/"
-            title="Garden store"
-            description="Online store(training project)"
-          />
-          <ProjectCard
-            src={pongGameLogo}
-            link="https://pong-game-p670.onrender.com"
-            title="The Pong Game"
-            description="JS,CSS,HTML(training project)"
-            className={styles.machine}
-          /> */}
         </div>
       </section>
     </div>
